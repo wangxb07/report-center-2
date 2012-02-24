f8d94fbfdd0f0aa9df250f9d17ea36fa51f8dbde
wangxb07-ubuntu <wangxb07@gmail.com>
2012-02-24 11:05:17 +0800

    修改sales_channel_apis，'增'、'查' 关联到sales_channel

--- app/models/sales_channel.rb
+++ app/models/sales_channel.rb
@@ -1,6 +1,6 @@
 class SalesChannel < ActiveRecord::Base
-  validates :name, :presence => true, :length => 5..30
+  validates :name, :presence => true, :length => 2..30
   validates :comment, :presence => true, :length => { :maximum => 255 }
 
-  has_many :sales_channel_apis, :dependent => :destroy
+  has_many :apis, :class_name => "SalesChannelApi", :dependent => :destroy
 end
--- app/models/sales_channel_api.rb
+++ app/models/sales_channel_api.rb
@@ -1,6 +1,6 @@
 class SalesChannelApi < ActiveRecord::Base
-  validates :app_key, :presence => true, :length => 5..255
-  validates :app_secret, :presence => true, :length => 5..255
+  validates :app_key, :presence => true, :length => 2..255
+  validates :app_secret, :presence => true, :length => 2..255
   validates :session_key, :length => 5..255
   validates :url, :length => 5..255
 
