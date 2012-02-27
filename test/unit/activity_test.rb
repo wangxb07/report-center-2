38a5128a7d520ced0826a3f10796e25846e0593d
wangxb07-ubuntu <wangxb07@gmail.com>
2012-02-27 13:48:56 +0800

    test activity save

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -21,7 +21,26 @@ class ActivityTest < ActiveSupport::TestCase
     activity_first.start_time = time
     activity_first.end_time = time
     activity_first.name = "first activity"
+    assert_equal false, activity_first.save
+
+    assert_equal("Products can't be blank",
+           activity_first.errors.full_message(:products, "can't be blank"))
+
+    assert_equal("Sales channel can't be blank",
+           activity_first.errors.full_message(:sales_channel, "can't be blank"))
+
+    activity_first.products_attributes = [
+      { :outer_sku => "1001111", :activity_price => "10" },
+      { :outer_sku => "1000222222" }
+    ]
+
+    activity_first.sales_channel = SalesChannel.new
+
     assert_equal true, activity_first.save
     assert_equal 100001, activity_first.increment_id
+
+    assert_equal 2, activity_first.products.count
+    assert_equal 10, ActivityProduct.find_by_outer_sku("1001111").activity_price
+    assert_equal 0, ActivityProduct.find_by_outer_sku("1000222222").activity_price
   end
 end
