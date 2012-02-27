d0691c9d9d7a5049356ecae44e7e287642dbb978
wangxb07-ubuntu <wangxb07@gmail.com>
2012-02-27 13:46:41 +0800

    validates unqiueness sku config

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true, :length => {:maximum => 255}
   validates :sku, :presence => true, :length => {:maximum => 255}
   validates :cost_price, :numericality => true
+
+  validates_uniqueness_of :sku
 end
